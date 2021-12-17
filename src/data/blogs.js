c61c2bc29d3c46ae2c7b91049cf9897047d77e95
experiencei <ayelojahighbee01@gmail.com>
2021-12-17 16:20:37 +0100

    made some changes

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -13,33 +13,33 @@ const blogs = [
     },
     {
         id: 2,
-        title: 'How To Use SEO Efficiently',
+        title: 'How to choose a programming Language',
         date: '01',
         month: 'April',
         image: blog2
     },
     {
         id: 3,
-        title: 'How to choose a programming Language',
+        title: 'How To Use SEO Efficiently',
         date: '01',
         month: 'April',
         image: blog3,
         link: 'https://www.google.co.uk/'
     },
-    {
-        id: 4,
-        title: 'How To Tse SEO Efficiently',
-        date: '01',
-        month: 'April',
-        image: blog4
-    },
-    {
-        id: 5,
-        title: 'How To Tse SEO Efficiently',
-        date: '01',
-        month: 'April',
-        image: blog4
-    }
+    // {
+    //     id: 4,
+    //     title: 'How To Tse SEO Efficiently',
+    //     date: '01',
+    //     month: 'April',
+    //     image: blog4
+    // },
+    // {
+    //     id: 5,
+    //     title: 'How To Tse SEO Efficiently',
+    //     date: '01',
+    //     month: 'April',
+    //     image: blog4
+    // }
 ]
 
 export default blogs;
